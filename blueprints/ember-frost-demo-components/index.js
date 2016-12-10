5e904da4cf4b3b7591dc1410dd87ee4d78c57f05
Sophy Pal <sophy.pal@gmail.com>
2016-12-09 20:30:01 -0800

    Update ivy-codemirror

--- blueprints/ember-frost-demo-components/index.js
+++ blueprints/ember-frost-demo-components/index.js
@@ -6,7 +6,7 @@ module.exports = {
       packages: [
         {name: 'ember-browserify', target: '^1.1.13'},
         {name: 'ember-frost-core', target: '^1.0.0'},
-        {name: 'ivy-codemirror', target: '^1.4.0'},
+        {name: 'ivy-codemirror', target: '^2.0.3'},
         {name: 'ember-cli-showdown', target: '^2.5.0'},
         {name: 'ember-prism', target: '>=0.0.7 <2.0.0'},
         {name: 'ember-frost-popover', target: '^4.0.0'}
