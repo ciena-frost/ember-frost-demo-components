3f99f139fbd0c70b4d2df2c6987862958c8a3379
Michael Carroll <juwara0@users.noreply.github.com>
2017-02-09 07:41:39 -0600

    updated usage of deprecated describeComponent()

--- tests/unit/components/frost-demo-editor-test.js
+++ tests/unit/components/frost-demo-editor-test.js
@@ -1,111 +1,96 @@
 /* globals describe, beforeEach, afterEach, Prism */
 
 import {expect} from 'chai'
-import {describeComponent, it} from 'ember-mocha'
 import sinon from 'sinon'
-import {registryHelper} from 'ember-test-utils'
 import resolver from '../../helpers/resolver'
+import {describe, it} from 'mocha'
 
-describeComponent(
-  'frost-demo-editor',
-  'Unit: EmberFrostDemoComponents - frost-demo-editor',
-  {
-    unit: true,
+import {unit} from 'dummy/tests/helpers/ember-test-utils/setup-component-test'
 
-    needs: [
-      'component:ivy-codemirror',
-      'component:markdown-to-html',
-      'component:frost-file-explorer',
-      'component:frost-file-node',
-      'component:frost-icon',
-      'helper:not',
-      'helper:and'
-    ],
-
-    beforeSetup () {
-      registryHelper.setup(resolver, {
-        'component:ivy-codemirror': Ember.Component.extend({}),
-        'component:markdown-to-html': Ember.Component.extend({}),
-        'component:frost-file-explorer': Ember.Component.extend({})
-      })
-    },
-
-    teardown () {
-      registryHelper.teardown()
-    }
-  },
-  function () {
-    let component, sandbox
-    beforeEach(function () {
-      sandbox = sinon.sandbox.create()
-      sandbox.spy(Prism, 'highlightAll')
-    })
-
-    afterEach(function () {
-      sandbox.restore()
-    })
-
-    describe('computed propety codeClass', function () {
-      it('computed property codeClass returns active when showCode is true', function () {
-        component = this.subject({
-          path: 'fullscreen',
-          showCode: true
-        })
-
-        expect(component.get('codeClass')).to.equal('active')
-      })
-
-      it('computed property codeClass returns active when showCode is false', function () {
-        component = this.subject({
-          path: 'fullscreen',
-          showCode: false
-        })
-
-        expect(component.get('codeClass')).to.equal('')
-      })
-    })
-
-    describe('computed property docClass', function () {
-      it('computed property docClass returns active when showCode is not true', function () {
-        component = this.subject({
-          path: 'fullscreen',
-          showCode: false
-        })
-
-        expect(component.get('docClass')).to.equal('active')
-      })
-
-      it('computed property docClass returns active when showCode is true', function () {
-        component = this.subject({
-          path: 'fullscreen',
-          showCode: true
-        })
-
-        expect(component.get('docClass')).to.equal('')
-      })
-    })
-
-    describe('afterRender', function () {
-      beforeEach(function () {
-        component = this.subject({
-          path: 'fullscreen',
-          showCode: false
-        })
-        this.render()
-      })
-
-      it('calls Prism.highlightAll', function () {
-        expect(Prism.highlightAll).to.have.been.called
-      })
-
-      // FIXME: get test passing again (MRD - 2017-01-10)
-      it.skip('sets up click events for the ribbon capsules', function (done) {
-        this.$('.ribbon .code').click()
-        Ember.run(() => {
-          expect(component.get('showCode')).to.equal(true)
-          done()
-        })
-      })
-    })
-  }
+const test = unit('frost-demo-editor',
+  [
+    'component:ivy-codemirror',
+    'component:markdown-to-html',
+    'component:frost-file-explorer',
+    'component:frost-file-node',
+    'component:frost-icon',
+    'helper:not',
+    'helper:and'
+  ]
 )
+describe(test.label, function () {
+  test.setup()
+
+  let component, sandbox
+  beforeEach(function () {
+    sandbox = sinon.sandbox.create()
+    sandbox.spy(Prism, 'highlightAll')
+  })
+
+  afterEach(function () {
+    sandbox.restore()
+  })
+
+  describe('computed propety codeClass', function () {
+    it('computed property codeClass returns active when showCode is true', function () {
+      component = this.subject({
+        path: 'fullscreen',
+        showCode: true
+      })
+
+      expect(component.get('codeClass')).to.equal('active')
+    })
+
+    it('computed property codeClass returns active when showCode is false', function () {
+      component = this.subject({
+        path: 'fullscreen',
+        showCode: false
+      })
+
+      expect(component.get('codeClass')).to.equal('')
+    })
+  })
+
+  describe('computed property docClass', function () {
+    it('computed property docClass returns active when showCode is not true', function () {
+      component = this.subject({
+        path: 'fullscreen',
+        showCode: false
+      })
+
+      expect(component.get('docClass')).to.equal('active')
+    })
+
+    it('computed property docClass returns active when showCode is true', function () {
+      component = this.subject({
+        path: 'fullscreen',
+        showCode: true
+      })
+
+      expect(component.get('docClass')).to.equal('')
+    })
+  })
+
+  describe('afterRender', function () {
+    beforeEach(function () {
+      component = this.subject({
+        path: 'fullscreen',
+        showCode: false
+      })
+      this.render()
+    })
+
+    it('calls Prism.highlightAll', function () {
+      expect(Prism.highlightAll).to.have.been.called
+    })
+
+    // FIXME: get test passing again (MRD - 2017-01-10)
+    it.skip('sets up click events for the ribbon capsules', function (done) {
+      this.$('.ribbon .code').click()
+      Ember.run(() => {
+        expect(component.get('showCode')).to.equal(true)
+        done()
+      })
+    })
+  })
+})
